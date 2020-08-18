516cec4fa980d396a328100c4069ab8ca738ad6b
Костя <GigaBOT4@yandex.ru>
2020-08-18 03:15:41 +0300

    Добавил js к фильтру

---
+++ source/js/filter.js
@@ -0,0 +1,46 @@
+var filterPart = document.querySelectorAll(".filter__part");
+var filterBtn = document.querySelector(".filter__btn-up");
+var filterBtnText1 = document.querySelector(".filter__btn-text-1");
+var filterBtnText2 = document.querySelector(".filter__btn-text-2");
+var filterParts = document.querySelector(".filter__parts");
+var filterGrid = document.querySelector(".filter__grid");
+var filterBtnDown = document.querySelector(".filter__btn-down");
+
+for (let i = 0; i < filterPart.length; i++) {
+  filterPart[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    var filterPartAct = document.querySelector(".filter__part--active");
+    filterPartAct.classList.remove("filter__part--active");
+    filterPart[i].classList.add("filter__part--active");
+  })
+}
+
+filterBtn.addEventListener("click", function () {
+  if (filterGrid.classList.contains("filter__grid--active")) {
+    filterBtnText2.classList.remove("filter__btn-text-active");
+    filterBtnText1.classList.add("filter__btn-text-active");
+  } else {
+    filterBtnText1.classList.remove("filter__btn-text-active");
+    filterBtnText2.classList.add("filter__btn-text-active");
+  }
+})
+
+filterBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (filterParts.classList.contains("filter__parts--active")) {
+    filterParts.classList.remove("filter__parts--active");
+    filterGrid.classList.remove("filter__grid--active");
+    filterBtnDown.classList.remove("filter__btn-down--active");
+  } else {
+    filterParts.classList.add("filter__parts--active");
+    filterGrid.classList.add("filter__grid--active");
+    filterBtnDown.classList.add("filter__btn-down--active");
+  }
+})
+
+filterBtnDown.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  filterParts.classList.remove("filter__parts--active");
+  filterGrid.classList.remove("filter__grid--active");
+  filterBtnDown.classList.remove("filter__btn-down--active");
+})
