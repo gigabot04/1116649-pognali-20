737a8a4ea37553ec3a50c422a4c527d695c39e77
Костя <GigaBOT4@yandex.ru>
2020-08-27 14:14:05 +0300

    СкроллМеню кроме десктопа

--- source/js/indexJs.js
+++ source/js/indexJs.js
@@ -2,6 +2,10 @@ var headerMenu = document.querySelector(".header-menu");
 var line1 = document.querySelector(".page-header__line1");
 var line2 = document.querySelector(".page-header__line2");
 var headerBtn = document.querySelector(".page-header__toggle");
+var headerTitle = document.querySelector(".header-menu__title");
+var headerAuth = document.querySelector(".header-menu__auth");
+var headerFeedback = document.querySelector(".header-menu__feedback");
+var headerSocial = document.querySelector(".header-menu__social-contacts");
 
 headerMenu.classList.remove("header-menu--jsnone");
 headerBtn.classList.remove("page-header__toggle--nojs");
@@ -15,11 +19,32 @@ headerBtn.addEventListener("click", function () {
   if (headerMenu.classList.contains("header-menu--active")) {
     headerMenu.classList.remove("header-menu--active");
     headerBtn.classList.remove("page-header__toggle--close");
+    headerTitle.classList.add("header-menu__title--none");
+    headerAuth.classList.add("header-menu__auth--none");
+    headerFeedback.classList.add("header-menu__feedback--none");
+    headerSocial.classList.add("header-menu__social-contacts--none");
   } else {
     headerMenu.classList.add("header-menu--active");
     headerBtn.classList.add("page-header__toggle--close");
+    headerTitle.classList.remove("header-menu__title--none");
+    headerAuth.classList.remove("header-menu__auth--none");
+    headerFeedback.classList.remove("header-menu__feedback--none");
+    headerSocial.classList.remove("header-menu__social-contacts--none");
   }
 })
+
+window.addEventListener("scroll", function () {
+  var pagePx = window.pageYOffset;
+
+  if (pagePx > 500) {
+    headerBtn.classList.add("page-header__toggle--fixed");
+    headerMenu.classList.add("header-menu--scrollMenu");
+  } else {
+    headerBtn.classList.remove("page-header__toggle--fixed");
+    headerMenu.classList.remove("header-menu--scrollMenu");
+  }
+})
+
 
 var authBtn = document.querySelector(".auth__btn");
 var authControl = document.querySelector(".auth__control");
